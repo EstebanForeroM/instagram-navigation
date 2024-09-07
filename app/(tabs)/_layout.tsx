4a58fa351ab57e4ebd787bc3497eaa0ab8df0ca7
Esteban Forero <estebanmff@outlook.com>
2024-09-07 17:49:07 -0500

    Layout finished

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,62 @@
-
-import { View, Text } from 'react-native'
 import React from 'react'
-import { Stack } from 'expo-router'
+import { Tabs } from 'expo-router'
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Octicons from '@expo/vector-icons/Octicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 const TabsLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name='home' options={{ headerShown: false}}/>
-      <Stack.Screen name='search' options={{ headerShown: false}}/>
-      <Stack.Screen name='create' options={{ headerShown: false}}/>
-      <Stack.Screen name='video' options={{ headerShown: false}}/>
-    </Stack>
+    <Tabs screenOptions={{
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: '#FF006E',
+      tabBarInactiveTintColor: '#CDCDE0',
+      tabBarStyle: {
+        backgroundColor: '#000000',
+        borderTopWidth: 1,
+        borderTopColor: '#000000',
+        height: 70
+      }
+    }}>
+
+      <Tabs.Screen name='home' options={{
+        headerShown: false,
+        title: 'Home',
+        tabBarIcon: ({color }) => (
+          <Feather name='home' size={24} color={color}/>
+        )
+      }}/>
+
+      <Tabs.Screen name='search' options={{
+        headerShown: false,
+        title: 'Search',
+        tabBarIcon: ({color }) => (
+          <FontAwesome5 name="search" size={24} color={color} />
+        )
+      }}/>
+
+      <Tabs.Screen name='create' options={{
+        headerShown: false,
+        tabBarIcon: ({color }) => (
+          <Ionicons name="add-circle" size={24} color={color} />
+        )
+      }}/>
+
+      <Tabs.Screen name='video' options={{
+        headerShown: false,
+        tabBarIcon: ({color }) => (
+          <Octicons name="video" size={24} color={color} />
+        )
+      }}/>
+
+      <Tabs.Screen name='profile' options={{
+        headerShown: false,
+        tabBarIcon: ({color }) => (
+          <MaterialIcons name="account-circle" size={24} color={color} />
+        )
+      }}/>
+    </Tabs>
   )
 }
 
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -7,9 +7,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const Home = () => {
   return (
     <SafeAreaView className='bg-background w-screen h-full'>
+      <HomeHeader/>
       <Text className='text-white'>Home</Text>
     </SafeAreaView>
   )
 }
 
 export default Home
+
+
+const HomeHeader = () => {
+  return (
+    <View className='mt-4 px-4 flex flex-row justify-between'>
+      <Text className='text-accent font-pbold text-2xl'>Night Flow</Text>
+      <View></View>
+    </View>
+  )
+}
